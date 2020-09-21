da20b4cab56068ec94475e273d9da10d060df7fe
Dheryasingh <63869048+Dheryasingh@users.noreply.github.com>
2020-09-21 11:15:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+function openCity(cityName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(cityName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
 function imageload()
 {
 	document.getElementById("editbox").style.display="block";
 }
-
 function showdnld()
 {
 	document.getElementById("step3").style.display="block";
 }
-
 function dropdown() {
   var x = document.getElementById("drpdwn");
   if (x.className.indexOf("w3-show") == -1) { 
@@ -35,4 +58,4 @@ function previewImageByURL(event){
 	var imageurl = document.getElementById("upurl").value
 	var img = document.getElementById("image-field")
 	img.src = imageurl
-}
+} 
