6b5b62e188bbb6e2bbf83dc089ddf95a9706a9e3
Pranjal Vishvakarma <55049003+AlwaysSmiling@users.noreply.github.com>
2020-11-24 17:29:28 +0530

    Done

--- OCR.py
+++ OCR.py
@@ -3,17 +3,21 @@ import pytesseract
 import io
 import os
 import sys
+from docx import Document
 from spellchecker import SpellChecker
+from itertools import zip_longest
 
 
 class SourceImg:
     def __init__(self):
-        self.srcimg = cv2.imread("source_raw.jpg")
+        self.srcimg = cv2.imread("Source_Raw.jpg")
         self.edit = self.srcimg
 
     def get_gray_noise(self):
         self.edit = cv2.cvtColor(self.edit, cv2.COLOR_BGR2GRAY)
-        self.edit = cv2.medianBlur(self.edit, 3)
+        self.edit = cv2.threshold(self.edit, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+
 
 
 lanstr = str(sys.argv[1])
@@ -32,14 +36,7 @@ if out == 'pdf':
 
 raw = pytesseract.image_to_string(img.edit,lang=lanstr) 
 
-with io.open('raw_output.txt', "w", encoding="utf-8") as f:
-    f.write(raw)
-
-
-with io.open('raw_output.txt', 'r', encoding='utf-8') as c:
-    content = c.read()
-
-content_list = content.split()
+content_list = raw.split()
 correct_list = []
 
 if lanstr == 'hin':
@@ -81,8 +78,32 @@ elif lanstr == 'eng':
 else:
     print('ERROR-- wrong language input')
     
+with io.open('final_output.txt', 'r', encoding='utf-8') as r:
+    final_text = r.read()
 
 
+raw_list = list(raw)
+for i in range(len(raw_list)-1):
+    if raw_list[i] == ' ' and raw_list[i+1] == ' ':
+        raw_list.pop(i+1)
+finl_list = list(final_text)
+end_list = []
+
+for i,j in zip_longest(raw_list,finl_list,fillvalue=' '):
+    if str(i) != '\n':
+        end_list.append(j)
+    elif str(i) == '\n':
+        if str(j) == ' ':
+            end_list.append(i)
+        else:
+            end_list.append(j)
+            end_list.append(i)
+
+finish = ''.join(end_list)
+
+docu = Document()
+docu.add_paragraph(finish)
+docu.save('final_doc.docx')
 
 
 
